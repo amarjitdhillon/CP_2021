eb13188ecb89ad4b18b1ef462a3c6323ae5e50cb
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-02 12:19:48 -0500

    Time: 57 ms (9.66%), Space: 14.3 MB (62.61%) - LeetHub

--- merge-sorted-array/merge-sorted-array.py
+++ merge-sorted-array/merge-sorted-array.py
@@ -3,11 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # we will take 3 pointers, x ,y and z
+        # we will take 3 pointers: x ,y and z to solve this issue
         x,y,z  = m-1,n-1,m+n-1
-        while (x > -2) and (y> -1):
-            if y< 0:
+        
+        # z index can go till 0
+        for z in range(m+n-1,-1,-1):
+            if y < 0:
                 break
+            # we are checking if x >= 0 as we are using x index here
             if x >=0 and (nums2[y] < nums1[x]):
                 nums1[z] = nums1[x]
                 # update the x pointer as x value is copied
