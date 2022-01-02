9e2299de2a5705ade9d6c298b09520c9f9e2d6d8
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-01 21:51:49 -0500

    Time: 109 ms (42.21%), Space: 15.1 MB (59.50%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,10 +1,13 @@
 from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ''' In this case we are using the 2 pass hashmap scheme. In the first pass every element is added as key and it's index is added as a value. For the second pass, we will check if the complement of this element exists in tehe hahmap, if it does then we will return the index of the element and it's complement '''
+        # first pass
         nums_hashmap = defaultdict(list)
         for x in range(len(nums)):
             nums_hashmap[nums[x]] = x
         
+        # second pass
         for y in range(len(nums)):
             complement = target - nums[y]
             if complement in nums_hashmap and nums_hashmap[complement] != y:
