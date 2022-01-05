d1576dd7833a851efe0ea93ec00f003e23cf9f2f
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-04 21:05:27 -0500

    Time: 42 ms (16.52%), Space: 14.4 MB (24.39%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -2,23 +2,24 @@ class Solution:
     def generate(self, n: int) -> List[List[int]]:
         if n == 0:
             return []
+
         
-        # if n == 1:
-        #     return [1]
-        
-        
-        # create a pascal triangle
+        # create a pascal triangle and later each row one by one
         pascal = []
         
         for x in range(n):
-            row = ["X" for x in range(x+1)]
+            # we will build a row first, the first row will be of size 1 and then 2, 3 4 etc
+            row = [None for x in range(x+1)]
+            
+            # the first and last cols are always 1
             row[0], row[-1] = 1,1 
             
+            # we have to modify the 1 to len(row)-1 rows using DP
             for y in range(1,len(row)-1):
                 row[y] = pascal[x-1][y-1] + pascal[x-1][y]
             
+            # append the row to triangle
             pascal.append(row)
-        print(pascal)
         return pascal
 
                 
