f9cf60f6adeab2ff9508c223997938008d0138a6
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-06 07:22:45 -0500

    Time: 56 ms (60.42%), Space: 15.6 MB (19.11%) - LeetHub

---
+++ 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        To find the loneny nodes, we can do the pre-order traversal and then can return the count if one of node is there while other is None
+        """
+        self.lonely = []
+        
+        def inorder(node):
+            # base condition
+            if not node:
+                return None
+            
+            if not node.left and node.right:
+                self.lonely.append(node.right.val)
+                
+            if not node.right and node.left:
+                self.lonely.append(node.left.val)
+            
+            inorder(node.left)
+            inorder(node.right)
+
+        
+        inorder(root)
+        return self.lonely
