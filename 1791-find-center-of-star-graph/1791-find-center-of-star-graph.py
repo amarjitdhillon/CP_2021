0148d4bf1cf033b8acb2d07439b8f80950a336e0
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-09 13:27:59 -0500

    Time: 1820 ms (5.05%), Space: 52.1 MB (5.76%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,18 +1,27 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        # if len(edges) == 0:
-        #     return None
+        """
+        - First we can create a adjacency list (as hashmap) from the given edges list. This will take o(n) time
+        - Then we can iterate over the adjacency list hashmap and calculate the degree of each vertex
+        - Output the node, which has degree as length of edges list
+        
+        Time complexity: O(N) as we are iterating
+        Space complexity:
+        """
         
         # create a adjacency list
         al = collections.defaultdict(list)
+        res = 0
         for x in edges:
             al[x[0]].append(x[1]) 
             al[x[1]].append(x[0])
-        
-        res = 0
-        for y in al:
-            if len(al[y]) == len(edges):
-                res = y
+            
+            if len(al[x[0]]) == len(edges):
+                res = x[0]
+                break
+                
+            if len(al[x[1]]) == len(edges):
+                res = x[1]
                 break
             
         return res
