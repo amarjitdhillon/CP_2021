e86e0f4e858aeb04c8255e17e4d25fa73bc91d9d
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-09 13:36:01 -0500

    Time: 1337 ms (8.59%), Space: 50.3 MB (74.86%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -8,24 +8,30 @@ class Solution:
         Time complexity: O(N) as we are iterating once in a graph
         Space complexity: O(N) for storing hashmap
         """
+        def using_hashmap():
+            # create a adjacency list
+            al = collections.defaultdict(list)
+            res = 0
+            for x in edges:
+                al[x[0]].append(x[1]) 
+                al[x[1]].append(x[0])
+
+                if len(al[x[0]]) == len(edges):
+                    res = x[0]
+                    break
+
+                if len(al[x[1]]) == len(edges):
+                    res = x[1]
+                    break
+
+            return res
         
-        # create a adjacency list
-        al = collections.defaultdict(list)
-        res = 0
-        for x in edges:
-            al[x[0]].append(x[1]) 
-            al[x[1]].append(x[0])
-            
-            if len(al[x[0]]) == len(edges):
-                res = x[0]
-                break
-                
-            if len(al[x[1]]) == len(edges):
-                res = x[1]
-                break
-            
-        return res
-        
+        f=edges[0][0] 
+        s=edges[0][1]
+        if(f in edges[1]):
+            return f
+        else:
+            return s
         
         
         
