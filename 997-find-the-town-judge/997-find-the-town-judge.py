795f8dd36e363862e9e2aa286a5c98e5e6876ee9
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-09 20:35:00 -0500

    Time: 736 ms (83.05%), Space: 19.1 MB (20.88%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        """
+        Solving this by using a 2d list of form [x,y]
+        Here index of this list is the ith person
+        x is the number of people this person trusts
+        y is the number of people who trust this guy
+        
+        Once we create this list, then we will loop through it to find the judge
+        The judge will be then person having [0, n-1] as it trusts no one and n-1 people trusts it
+        """
+        # special case
+        
+        if n == 1 and len(trust) == 0:
+            return 1
+        
+        link = [[0,0] for x in range(n+1)]
+        for x,y in trust:
+            link[x][0] = link[x][0] + 1
+            link[y][1] = link[y][1] + 1
+        
+        print(link)
+        judge = -1
+        for x in range(n+1):
+            if link[x][0] == 0 and link[x][1] == n-1:
+                judge = x
+                break
+        return judge
