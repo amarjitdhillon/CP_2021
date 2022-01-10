c5d483a8c77c28cf174d8bb450d7f0e3ac713a6f
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-09 20:36:39 -0500

    Time: 1164 ms (36.98%), Space: 19 MB (62.32%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -10,19 +10,18 @@ class Solution:
         The judge will be then person having [0, n-1] as it trusts no one and n-1 people trusts it
         """
         # special case
-        
         if n == 1 and len(trust) == 0:
             return 1
         
-        link = [[0,0] for x in range(n+1)]
-        for x,y in trust:
-            link[x][0] = link[x][0] + 1
-            link[y][1] = link[y][1] + 1
+        link = [[0,0] for x in range(n+1)] # create a emppty link array
         
-        print(link)
+        for x,y in trust:
+            link[x][0] = link[x][0] + 1 # as this guy trusts one more person
+            link[y][1] = link[y][1] + 1 # as this guy is trusted by one more person
+        
         judge = -1
         for x in range(n+1):
-            if link[x][0] == 0 and link[x][1] == n-1:
+            if link[x][0] == 0 and link[x][1] == n-1: #condition for a judge
                 judge = x
                 break
         return judge
