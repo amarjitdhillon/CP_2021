64dc923e8d9153f2f11c81f6ab9a35f85633f004
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-24 22:39:50 -0500

    Time: 362 ms (52.68%), Space: 16.9 MB (40.14%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        num_rows,num_cols = len(grid), len(grid[0])
+        
+        x = 0
+        def findIsland(r,c):
+            
+            if grid[r][c] == "x":
+                return
+
+            if grid[r][c] == "1":
+                grid[r][c] = "x"
+                
+            # find right 
+            if c+1 > -1 and c+1 < num_cols and grid[r][c+1] == "1":
+                findIsland(r,c+1)
+            
+            # call left
+            if c-1 > -1 and c-1 < num_cols and grid[r][c-1] == "1":
+                findIsland(r,c-1)
+                
+            # call top
+            if r-1 > -1 and r-1 < num_rows and grid[r-1][c] == "1":
+                findIsland(r-1, c)
+            
+            # call bottom
+            if r+1 < num_rows and r+1 > 0 and grid[r+1][c] == "1":
+                findIsland(r+1, c)
+                        
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if grid[row][col] == "1":
+                    findIsland(row,col)
+                    x += 1
+        return x
+        
+            
