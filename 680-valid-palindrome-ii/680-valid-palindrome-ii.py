1b9b3c9ddcf2ed0e4a70a3576b07424d8533edbd
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-27 12:59:43 -0500

    Time: 306 ms (14.00%), Space: 14.5 MB (91.96%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,25 +1,21 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        # if len(s) == 2:
-        #     return True
-        
-        l, h = 0, len(s)-1
+        l, h = 0, len(s)-1 # use 2 pointers
   
-        def isPalindrome(i,j):
+        def isPalindrome(i,j): # used to check in any string is a palindrome
             while i < j :
                 if s[i] == s[j]:
                     i += 1
                     j -= 1
                 else:
-                    return False
+                    return False # as chars don't match, so we return False
             return True
-        
         
         while (l<h):
             if s[l] == s[h]:
                 l += 1
                 h -= 1
-            else:
-                return isPalindrome(l+1, h) or isPalindrome(l, h-1)
+            else: # we will consider 2 cases, in first we will skip the l^th characher and in other we will skip h^th char
+                return isPalindrome(l+1, h) or isPalindrome(l, h-1) # if either of then return true, then string is a palindrome
         return True
                 
