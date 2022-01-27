4de7bc57a0e54561e03b63c8123d5684f8a80d6c
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-27 16:34:00 -0500

    Time: 1570 ms (5.32%), Space: 16.4 MB (24.29%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,6 +1,18 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        stack = []
+        '''
+        this is a 3 pass solution with 2 pass for the string and one pass for the stack to get the index 
+        if we encounter closing brace, then we will not apend it in stack and remove the opening brace tuple from stack
+        if we encounter a opening brace, we will add it to stack along with it's index
+        
+        After the first iteration, the stack will contain all the braces which needs to be removed
+        so we will iterate over the stack and get all the index which needs to be removed
+        
+        In the third iteration, we will loop over the string and not include the index values which are present in the stack
+        Finally join the elements in stack and return the string
+        '''
+        stack = [] # this will hold the braces
+        
         open_brace, close_brace = '(', ')'
         for i in range(len(s)):
             char = s[i]
@@ -12,15 +24,16 @@ class Solution:
                 else:  # add the close brace
                     stack.append([char,i])
         
-        extra = []
-        res = []
+        extra = [] # contain ideex of elements to be removed
+        res = []   # final list of chars to be returned
         
         for [x,y] in stack:
-            extra.append(y)
+            extra.append(y) 
             
         for i in range(len(s)):
-            if i in extra:
+            if i in extra: # skip this char
                 continue
             res.append(s[i])
-        return "".join(res)
+            
+        return "".join(res) # return the chars as string
                 
