a6948e5d8789273aa0b169145c6d362fd07c144a
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-01-30 20:25:52 -0500

    Time: 68 ms (5.04%), Space: 13.8 MB (98.17%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -2,7 +2,7 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
         while(n > 0):
-            if n&1 == 1:
+            if n&1 == 1:   #means the last bit is one, so increase the counter
                 count += 1
-            n >>= 1
+            n >>= 1         # right shift by one, to process the next bit
         return count
