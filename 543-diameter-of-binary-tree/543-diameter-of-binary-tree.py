677351ae4fa7f1ddaeb0ac12f9456c8ae349b3a4
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-03 14:57:17 -0500

    Time: 96 ms (9.09%), Space: 16.3 MB (71.10%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -6,21 +6,20 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        dia = 0
-        
+        dia = 0       # we will make this variable as global using non-local
         def dfs(root):
             nonlocal dia # a non local variable
             
-            if not root:
+            if not root: # null case
                 return 0
             
             lh = dfs(root.left)
             rh = dfs(root.right)
             
-            dia = max(dia, lh+rh)
+            dia = max(dia, lh+rh)  # update the diameter but don't return it
             
-            return 1 + max(lh,rh) 
+            return 1 + max(lh,rh)  # return the depth of subtree
         
-        dfs(root)
+        dfs(root)  # calling this fn will update the diameter of the tree
         return dia
         
