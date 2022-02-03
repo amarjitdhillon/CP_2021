bc1ca9e8f017236b2175badfe198842e1145f2c6
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-03 16:04:58 -0500

    Time: 2809 ms (39.89%), Space: 106.4 MB (76.83%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -0,0 +1,42 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        
+        # if the edges list is empty then return False
+        if len(edges) in [0,1]:
+            return True
+        
+        # build the adjacency list
+        edgeDict = collections.defaultdict(list)
+        for x,y in edges:
+            edgeDict[x].append(y)
+            edgeDict[y].append(x)
+            
+        
+        # Edge case: check if either of the vertex is not in the graph
+        if not start in edgeDict or not end in edgeDict:
+            return False
+        
+        visited = [False] * (len(edgeDict)+10)
+        queue = [start]
+        
+        while queue:
+            node = queue.pop(0)  # take out the node which we want to process
+            
+            # mark it as visited
+            visited[node] = True
+            
+            if end in edgeDict[node]:
+                return True
+            
+            # add all the neighbours to the queue for processing
+            for val in edgeDict[node]:
+                if visited[val] == False: # only add the node if it is not visited
+                    queue.append(val)
+                
+        return False
+        
+        
+            
+        
+        
+        
