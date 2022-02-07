089eb10063d7ebf414ff5cf209e7275afcef32b7
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-07 00:48:55 -0500

    Time: 1209 ms (23.27%), Space: 27.4 MB (73.94%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         '''
-        - It's given that height.length > 2, so we do not need consider n == 1 case
-        - As we want to find the max water, so initiaize l and r pointers at 0 and len(height)-1 resp
-        - Set maxArea = 0
-        - While l< r, compute maxArea with given l and r values
-        - currArea = height * width = min(l,r) * (r-l+1)
-        - if currArea  > maxArea, then update maxArea = currArea
-        - return maxArea at end
-        
-        Time complexity: O(N) as both pointers will stop at l<r
-        Space complexity = O(1) as vaiables are used which takes constant time
+            - It's given that height.length > 2, so we do not need consider n == 1 case
+            - As we want to find the max water area, so initialize l and r pointers at 0 and length(height)-1 respectively- 
+            - Width between 2 consecutive nums = 1
+            - Set maxArea = 0 initially
+            - While l< r and (l and r are in bounds), compute currArea with given l and r values. 
+            - Here currArea represents currentArea given l and r as pointers holding water.
+            - currArea = height * width = min(height[l],height[r]) * (r-l)
+            - If currArea > maxArea, then update maxArea = currArea
+            - return maxArea at the end
         '''
         maxArea, currArea, l, r = 0,0, 0, len(height)-1
         
