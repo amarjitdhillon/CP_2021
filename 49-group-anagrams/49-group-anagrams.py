45446cc1a9f72d986f7f74e9f3ed3b8ca4ff2d0c
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-06 20:37:30 -0500

    Time: 112 ms (68.49%), Space: 19.7 MB (19.58%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,10 +1,12 @@
 class Solution:
     """
-    - As each char consists of lowercase English letters,so we can use the list of 26 lengh to represent each word
-    - Create a hashmap of type list:list, where key is list of lengh 26 frequency count and values is a list of word which match that freq count 
-    - If two lists match, then append the word to the end of this values list in hashmap
-    - If that key of list is not present in the hashmap, then create a new key and value pair.
-    - At the end, append the list values of hashmap to result list and return result list
+    - As each char consists of lowercase English letters, so we can use the list of 26 lengths to represent each word
+    - Loop over each word in the given input list
+    - Create a hashmap of type {tuple: list}, where the key is a tuple of frequency count of length 26 representing each char of English alphabet and values is a list of anagrams for that tuple 
+    - If two frequency tuples match, then append the current word to the end of this values list in the hashmap
+    - f the tuple key is not present in the hashmap, then create a new key(tuple) and value pair (current word).
+    - If the tuple key is present, then add the current word to the values list of the hashmap.
+    - At the end, return the hashmap.values()
         """
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:        
         if len(strs) == 0:
@@ -16,14 +18,14 @@ class Solution:
             freq_list = [0]*26
             
             for c in word:
-                freq_list[ord(c)-ord('a')] += 1   
+                freq_list[ord(c)-ord('a')] += 1    # increment counter for freq in list
             
-            freq_list = tuple(freq_list)
+            freq_list = tuple(freq_list) # as tuple is immutable and can be used as key in dict
             
             if freq_list not in c_dict.keys():
-                c_dict[freq_list] = [word]
+                c_dict[freq_list] = [word]     # add the first word in the list
             else:
-                c_dict[freq_list].append(word)
+                c_dict[freq_list].append(word)  # add more anagrams to the list
                 
         return c_dict.values()
         
