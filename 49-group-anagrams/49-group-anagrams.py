d721e90912e962284e1ef92a47a03b47cb43552e
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-06 19:46:53 -0500

    Time: 166 ms (31.68%), Space: 19 MB (26.11%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,36 @@
+class Solution:
+    """
+    - As each char consists of lowercase English letters,so we can use the list of 26 lengh to represent each word
+    - Create a hashmap of type list:list, where key is list of lengh 26 frequency count and values is a list of word which match that freq count 
+    - If two lists match, then append the word to the end of this values list in hashmap
+    - If that key of list is not present in the hashmap, then create a new key and value pair.
+    - At the end, append the list values of hashmap to result list and return result list
+        """
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        
+        if len(strs) == 0:
+            return res
+        
+        c_dict = {}
+        
+        for word in strs:
+            freq_list = [0]*26
+            
+            for c in word:
+                freq_list[ord(c)-ord('a')] += 1   
+                
+            if tuple(freq_list) not in c_dict.keys():
+                c_dict[tuple(freq_list)] = [word]
+            else:
+                c_dict[tuple(freq_list)].append(word)
+                
+        
+        for val in c_dict.values():
+            res.append(val)
+        
+        return res
+        
+        
+        
+        
