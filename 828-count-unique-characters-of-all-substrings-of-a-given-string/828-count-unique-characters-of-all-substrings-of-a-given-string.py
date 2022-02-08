3268dc307ded529bb95cf4a81859b43fe7aa8cab
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-08 12:42:49 -0500

    Time: 478 ms (41.20%), Space: 19.5 MB (9.18%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.py
@@ -1,15 +1,19 @@
 class Solution:
     def uniqueLetterString(self, s: str) -> int:
-        record = [[] for _ in range(26)]
+        record = [[] for _ in range(26)]    # for all english characters
+        
         # record all the index
-        for i,letter in enumerate(s):
-            record[ord(letter) - 65].append(i)
-
-        ans = 0
-        # avoid searching the letters that don not appear in the string, and adding the boundary index.
+        for index,character in enumerate(s):
+            record[ord(character) - 65].append(index)
+            
+        result = 0
+        # avoid searching the letters that do not appear in the string, and adding the boundary index.
         check_list = [[-1]+index+[len(s)] for index in record if index]
+        
         for index in check_list:
             for i in range(1,len(index)-1):
-                ans+=(index[i] - index[i-1])*(index[i+1] - index[i])
+                left = index[i] - index[i-1]
+                right = index[i+1] - index[i]
+                result += left* right
 
-        return ans
+        return result
