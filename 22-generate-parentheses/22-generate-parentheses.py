03e727e2d6326138d112c99609d16a8ee2ecb049
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-08 02:26:37 -0500

    Time: 52 ms (41.47%), Space: 14.3 MB (89.76%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -8,15 +8,16 @@ class Solution:
         def generateParanthesis(open_count,close_count,r):
             
             if open_count == close_count == n:  # base case, bottom of the tree
-                result.append("".join(r))       # add it to the final result
+                result.append("".join(r))       # add the parantehsis list to the final result after converting it to string
                 return
             
-            if open_count < n :
+            if open_count < n : # as open brace can be added as long as it's count is less then what is allowed
                 r.append("(")
                 generateParanthesis(open_count+1,close_count,r)
-                r.pop() # to make it to the old state as we need to add ( again
+                r.pop()         # to make it to the old state as we need to add ( again
                 
-            if close_count < open_count:
+            
+            if close_count < open_count: # closing brace can only be added if opening brace is there which means the count of closing brace will always be lesser then opening brace
                 r.append(")")
                 generateParanthesis(open_count,close_count+1,r)
                 r.pop()
