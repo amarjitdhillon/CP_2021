accc66c437df3779fc1a2992a6a4da053dd86139
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-08 17:45:25 -0500

    Time: 434 ms (39.05%), Space: 15 MB (79.44%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -1,13 +1,13 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        oneCount, changes = 0,0                 # intially both are 0
+        oneCount, flipCount = 0,0                 # intially both are 0
         for c in s:
             if c == "1":
                 oneCount += 1
-            else:                               # if not 1 then it's a zero
+            else:                                   # if not 1 then it's a zero
                 if oneCount >= 1:       
-                    changes += 1                # in case we have already seen a zero, we will increase the changes count
-            changes = min(changes, oneCount)    # we will do changes if it's count is less then # of 1's seen so far
-        return changes
+                    flipCount += 1                  # in case we have already seen a zero, we will increase the flipCount 
+            flipCount = min(flipCount, oneCount)    # we will do fip if it's count is less then # of 1's seen so far
+        return flipCount
     
         
