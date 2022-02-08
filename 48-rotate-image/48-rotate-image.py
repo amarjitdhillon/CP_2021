b0d965964fc1e15ccdff8ef3d66b1f6b02dc503f
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-07 23:31:14 -0500

    Time: 71 ms (8.92%), Space: 13.9 MB (96.52%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,25 +1,22 @@
 class Solution:
     def rotate(self, mat: List[List[int]]) -> None:
         """
-        Do not return anything, modify matrix in-place instead.
+        Time complexity: O(N) as we are iterating the matrix twice where N is the number of cells in the grid.
+        Space complexity: O(1) as  we are doing the in place transaction
         """
-
         C = len(mat[0])
         R = len(mat)
         
-        # first transformation
+        # first transformation is for all the rows and half of the element lying to right hand side of diagonal
         for r in range(R):
             for c in range(r,C):
-                temp = mat[r][c]
-                mat[r][c] = mat[c][r]
-                mat[c][r] = temp
+                mat[r][c], mat[c][r] = mat[c][r], mat[r][c]
+
                 
-        # second transformation
+        # second transformation is for all the rows and all elements lying to left of the middle column.
         for r in range(R):
             for c in range(0,C//2):
-                temp = mat[r][c] 
-                mat[r][c] = mat[r][C-c-1]
-                mat[r][C-c-1] = temp
+                mat[r][c], mat[r][C-c-1] = mat[r][C-c-1], mat[r][c]
                 
         return mat
         
