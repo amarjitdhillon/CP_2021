e31b7fc6fb041c67eb264880decaea5da8f59214
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-08 02:53:11 -0500

    Time: 69 ms (15.67%), Space: 14.3 MB (89.76%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -5,23 +5,23 @@ class Solution:
         
         open_count,close_count, result, r = 0, 0, [], []
         
-        def generateParanthesis(open_count,close_count,r):
+        def dfsParantheses(open_count,close_count,r):
             
             if open_count == close_count == n:  # base case, bottom of the tree
-                result.append("".join(r))       # add the parantehsis list to the final result after converting it to string
+                result.append("".join(r))       # add the parentheses list to the final result after converting it to string
                 return
             
             if open_count < n : # as open brace can be added as long as it's count is less then what is allowed
                 r.append("(")
-                generateParanthesis(open_count+1,close_count,r)
+                dfsParantheses(open_count+1,close_count,r)
                 r.pop()         # to make it to the old state as we need to add ( again
                 
             
             if close_count < open_count: # closing brace can only be added if opening brace is there which means the count of closing brace will always be lesser then opening brace
                 r.append(")")
-                generateParanthesis(open_count,close_count+1,r)
+                dfsParantheses(open_count,close_count+1,r)
                 r.pop()
                 
-        generateParanthesis(0,0,r) # call recursive function
+        dfsParantheses(0,0,r) # call recursive function
 
         return result
