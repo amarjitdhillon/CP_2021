c81f353fb2ca741852c52965b705695338110052
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-08 01:02:08 -0500

    Time: 197 ms (27.83%), Space: 18 MB (46.63%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -6,31 +6,33 @@
 import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # create a min heap and add the first element of each LL with the id. here `id` is the list number
+        # create a min heap and add the first element of each linked list with the id. here `id` is the list number
         minheap = []
         
-        # add the first elemnets in minheap
+        # add the first elements of each linked list in the minheap
         for i,ll in enumerate(lists): # i is index and ll in linked list
             if ll:
                 heapq.heappush(minheap, (ll.val,i))
             
-        # create a root node
-        root = ListNode()
-        curr = root
         
-        # extract min element
+        root = ListNode()  # create a root node for the output linked list
+        curr = root     
+        
+        # extract min element from each linked list until all of the linked list are not traversed. 
+        # We will know all of them are traversed once the minheap is empty as each node in minheap holds a reference to the ll element
         while minheap:
-            val, i = heapq.heappop(minheap)
+            val, i = heapq.heappop(minheap) # get the min element and it's list #
             
-            # add it to the result linked list
+            # add the min node to the result linked list and update the current pointer
             curr.next = ListNode(val)
             curr = curr.next
             
-            # update the heap pointer based on the index of the linked list
+            # update the linked list pointer based on the index of the linked list. Here i is the index of the current LL having lowest value
             lists[i] = lists[i].next 
             
-            # udate the heap with next value
+            # after updating the pointer to the linked list, if it's not null, then we will update the heap pointer
+            # if the ll is pointing to null, then no need to update heap as we have already poped the lowest node element.
             if lists[i]: 
                 heapq.heappush(minheap, (lists[i].val,i))
             
-        return root.next
+        return root.next # as root was a dummy node
