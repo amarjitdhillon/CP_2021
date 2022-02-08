266aa20307156d12b5f5b5619e971f2c14497a6a
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-08 14:36:50 -0500

    Time: 46 ms (53.45%), Space: 14.1 MB (87.53%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -1,16 +1,20 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        res = []
-        result  = []
+        result, plogs = [], []
+        
         for i, log in enumerate(logs):
-            lg = log.split(" ")
-            if lg[1].isalpha(): #is a word
-                res.append((0,lg[1:], lg[0],i))   # tuple with 3 keys
+            lg = log.split(" ")  # split the string over the space and convert to list
+            
+            if lg[1].isalpha(): # if this is a word or not: this can be checked if 2nd element is string or not?
+                # tuple with 4 keys (k1,k2,k3,k4)
+                # k1 for which type of logs, k2 for contents of logs, k3 for log identifier and k4 for original index in logs list
+                plogs.append((0,lg[1:], lg[0],i))   
             else:
-                res.append((1,None, None,i))
+                plogs.append((1,None, None,i))  # here k2 and k3 are not required as we do not need to sort if it's a digit
                 
-        res = sorted(res)
-        for x in res:
-            index = x[3]
-            result.append(logs[index])
+        plogs = sorted(plogs)
+        
+        for plog in plogs:
+            index = plog[3]              # get the index of this string in the original logs array
+            result.append(logs[index])   # add the string to result list based on the index we got
         return result
