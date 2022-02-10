1fd2af63e34358e748dd3c00e62badbaf975c1a5
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-10 12:09:33 -0500

    Time: 208 ms (36.05%), Space: 14.5 MB (91.05%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -1,17 +1,23 @@
 class ParkingSystem:
     # initialize the parking object
     def __init__(self, big: int, medium: int, small: int):
-        self.lot = {}
+        # to hold various types of parking we are using a dict in constructor
+        self.lot = {}  
+        
+        # add the parking counters in this lot dict
         self.lot[1] = big
         self.lot[2] = medium
         self.lot[3] = small
         
     # if car can be parked then return True else False
     def addCar(self, carType: int) -> bool:
-        if self.lot.get(carType) > 0:
+        
+        # if it has the space, then park the car and update the space
+        if self.lot.get(carType) > 0: 
             self.lot[carType] -= 1
             return True
         else:
+            # return False if car can not be parked
             return False
 
 
