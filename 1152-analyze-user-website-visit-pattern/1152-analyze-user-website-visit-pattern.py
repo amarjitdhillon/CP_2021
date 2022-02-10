fa4a79680061fd16084366b7025f911302351bf3
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-10 18:43:17 -0500

    Time: 63 ms (61.59%), Space: 14.8 MB (55.26%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -7,22 +7,32 @@ class Solution:
         # we need to sort them by time --> username --> website
         sorted_tuw = sorted(tuw)
 
-        # get a user history hashmap of various pages visited. The hashmap is of type {user: [pages]}
+        # We will polulate a user history hashmap of various pages visited. The hashmap is of type {user: [pages]}
         user_history = defaultdict(list)
         for t, u, w in sorted_tuw:
             user_history[u].append(w)
 
-        # print("user_history= ", user_history)
-
         # get various combinations possible for various users
         patternCount = defaultdict(int)
         for user,pages in user_history.items():
-            comb = set(combinations(pages,3))
-            for c in comb:
-                patternCount[c] += 1
+            
+            # get various combinations possible for various users in the pair of 3 and add them to set so that they are unique
+            userCombinations = set(combinations(pages,3))
+            
+            # for every pair of userCombination, we will update the count. This count will make sense for second user onwards having the same pattern
+            for userCombination in userCombinations:
+                patternCount[userCombination] += 1
         
-        values = [-x for x in patternCount.values()]
-        c = list(zip(values, patternCount.keys()))
+        # We need to sort both the keys(pattern lexographically) from from min to max and values(score) form max to min 
+        # If we inverse the values of keys from val to -val, then we can sort both of them in ascending order, using minhea
+        # here invertedValues represent the values with -ve sign added to it
+        invertedValues = [-x for x in patternCount.values()]
+        
+        # Zip both the keys and values so that we can sort them. We are placing invertedValues first as we first need to sort by score and then need to sort lexographically in natural order
+        c = list(zip(invertedValues, patternCount.keys()))
+        
+        # Heapify will sort them in the ascending order as this is min heapify operation and will take nlog(n) time
         heapq.heapify(c)
 
+        # Top of the heap will represent the sorted value by -ve score, we can return the second element from this popped element 
         return heapq.heappop(c)[1]
