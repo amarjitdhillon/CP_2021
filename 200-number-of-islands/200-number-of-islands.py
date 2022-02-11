7e36dc149fffb4df18990230a6e9b536d2bcd1db
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-11 17:52:44 -0500

    Time: 388 ms (58.40%), Space: 25.8 MB (5.63%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,33 +1,35 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_rows,num_cols = len(grid), len(grid[0])
+        num_rows,num_cols,num_islands = len(grid), len(grid[0]),0
+        visited = set()
         
-        x = 0
         def findIsland(r,c):
-            # this function is called only if the value is equal to 1, so we set it to something else
-            grid[r][c] = "x"
+            visited.add((r,c))
                 
-            # find right 
-            if c+1 < num_cols and grid[r][c+1] == "1":
+            # find right neighbour value
+            if c+1 < num_cols and grid[r][c+1] == "1" and (r,c+1) not in visited:
                 findIsland(r,c+1)
             
-            # call left
-            if c-1 > -1 and grid[r][c-1] == "1":
+            # find left neighbour value
+            if c-1 >= 0 and grid[r][c-1] == "1" and (r,c-1) not in visited:
                 findIsland(r,c-1)
                 
-            # call top
-            if r-1 > -1 and grid[r-1][c] == "1":
+            # find top neighbour value
+            if r-1 >= 0 and grid[r-1][c] == "1" and  (r-1,c) not in visited:
                 findIsland(r-1, c)
             
-            # call bottom
-            if r+1 < num_rows and grid[r+1][c] == "1":
+            #  find bottom neighbour value
+            if r+1 < num_rows and grid[r+1][c] == "1" and (r+1,c) not in visited:
                 findIsland(r+1, c)
-                        
+                
+
+                 
+        # driver code
         for row in range(num_rows):
             for col in range(num_cols):
-                if grid[row][col] == "1":
+                if grid[row][col] == "1" and (row,col) not in visited:
                     findIsland(row,col)
-                    x += 1
-        return x
+                    num_islands += 1
+        return num_islands
         
             
