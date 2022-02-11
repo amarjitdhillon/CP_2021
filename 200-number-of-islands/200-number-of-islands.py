89e875e73702485aca2d6041b8b9d437beffcd27
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-11 18:09:58 -0500

    Time: 394 ms (56.76%), Space: 22.2 MB (10.44%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,31 +4,26 @@ class Solution:
         visited = set()
         
         def findIsland(r,c):
+            # add this cell to the visited set
             visited.add((r,c))
-                
-            # find right neighbour value
-            if c+1 < num_cols and grid[r][c+1] == "1" and (r,c+1) not in visited:
-                findIsland(r,c+1)
             
-            # find left neighbour value
-            if c-1 >= 0 and grid[r][c-1] == "1" and (r,c-1) not in visited:
-                findIsland(r,c-1)
-                
-            # find top neighbour value
-            if r-1 >= 0 and grid[r-1][c] == "1" and  (r-1,c) not in visited:
-                findIsland(r-1, c)
+            # add left, right, top and down respectively
+            neighbours = [[0,-1], [0,1],[-1,0],[1,0]]
             
-            #  find bottom neighbour value
-            if r+1 < num_rows and grid[r+1][c] == "1" and (r+1,c) not in visited:
-                findIsland(r+1, c)
+            for row_id,col_id in neighbours:
                 
-
+                # Visit a partiular neighbour if the index values are within bounds and the value at neighbour == 1 and that cell is not already visited
+                if 0 <= (c+col_id) < num_cols and 0 <= (r+row_id ) < num_rows and grid[r+row_id][c+col_id] == "1" and (r+row_id,c+col_id) not in visited:
+                    findIsland(r+row_id,c+col_id)
                  
         # driver code
         for row in range(num_rows):
             for col in range(num_cols):
+                # call each cell element if that is not already visited
                 if grid[row][col] == "1" and (row,col) not in visited:
                     findIsland(row,col)
+                    
+                    # increment the count if findIsland() returned it's execution as it means one set of continuous cells have been traversed
                     num_islands += 1
         return num_islands
         
