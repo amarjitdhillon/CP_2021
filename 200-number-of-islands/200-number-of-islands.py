bde1a5ff2239bddef06108397183b07c9760e411
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-11 18:12:38 -0500

    Time: 483 ms (35.89%), Space: 22.2 MB (10.44%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,18 +3,17 @@ class Solution:
         num_rows,num_cols,num_islands = len(grid), len(grid[0]),0
         visited = set()
         
-        def findIsland(r,c):
+        def findIsland(row,col):
             # add this cell to the visited set
-            visited.add((r,c))
+            visited.add((row,col))
             
-            # add left, right, top and down respectively
+            # add left, right, top and down neighbours of this cell respectively. Here the first element is row id and second one is the column id
             neighbours = [[0,-1], [0,1],[-1,0],[1,0]]
             
             for row_id,col_id in neighbours:
-                
-                # Visit a partiular neighbour if the index values are within bounds and the value at neighbour == 1 and that cell is not already visited
-                if 0 <= (c+col_id) < num_cols and 0 <= (r+row_id ) < num_rows and grid[r+row_id][c+col_id] == "1" and (r+row_id,c+col_id) not in visited:
-                    findIsland(r+row_id,c+col_id)
+                # Visit a partiular neighbour if the index values are within bounds and the string value stored at neighbour == "1: and that cell is not already visited
+                if 0 <= (col+col_id) < num_cols and 0 <= (row+row_id ) < num_rows and grid[row+row_id][col+col_id] == "1" and (row+row_id,col+col_id) not in visited:
+                    findIsland(row+row_id,col+col_id)
                  
         # driver code
         for row in range(num_rows):
