097ba58152e5b25f9d8a5976bfd51322318c0c3a
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-11 02:45:59 -0500

    Time: 148 ms (59.67%), Space: 19.1 MB (10.20%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,21 +1,28 @@
 class Solution:
-    def merge(self, i: List[List[int]]) -> List[List[int]]:
-
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
-        i.sort(key = lambda x:x[0])  # sort by the first value
-        c = i[0]    # start limit
-        r = []      # result array
+        # sort the intervals by the start value, in case they are not sorted by default
+        intervals.sort(key = lambda x: x[0])
         
-        for x in i:
-            if x[0] >= c[0] and x[0] <= c[1]: # this starting values lies in the range of prev tuple
-                c[0] = min(c[0],x[0])
-                c[1] = max(c[1],x[1])        
-                continue
+        # edge case. If num elements is one, then return the same list
+        if len(intervals) == 1:
+            return intervals
+        
+        # add the first element to the result list
+        result = [intervals[0]]
+        
+        # loop over the second element onwards as first is already added to result list
+        for start, end in intervals[1:]:
+            
+            # get the prev end value from the result array
+            prevEnd = result[-1][1]
+            
+            # if the start of this element is <= the prevEnd, then take the max from both's end and expand the interval
+            if  start <= prevEnd:
+                # update the end in result
+                result[-1][1] = max(end,prevEnd)
             else:
-                r.append(c) # append the previous value
-                c = x
+                result.append([start,end])
                 
-        r.append(c) # add the last value
-        return r
-        
-        
+        # else return the result
+        return result
