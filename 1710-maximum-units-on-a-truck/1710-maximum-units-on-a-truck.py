f20dcf8d70f154ec7e49095e3d82545937022339
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-12 04:53:04 -0500

    Time: 992 ms (12.91%), Space: 14.4 MB (97.00%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,14 +1,20 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        # sort the truck as per the second item in the boxTypes in the reverse order
         sortedBoxTypes = sorted(boxTypes, key= lambda x:x[1],reverse = True )
+        
+        # initialize variables
         pending, number_of_units = truckSize, 0
         
-        print(sortedBoxTypes)
+        # loop over the sortedBoxTypes
         for count, cost in sortedBoxTypes:
             if (pending > 0):
+                # If we still have the space, we will add the boxes one by one
                 for i in range(1,count+1):
                     if pending  > 0:
+                        # update the result
                         number_of_units += cost   
+                        # decrement the counter
                         pending -= 1
             else:
                 return number_of_units
