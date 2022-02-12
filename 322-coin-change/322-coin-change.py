adbe5ff8b69d58c4bd6e229cb6241eda0e0bea48
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-12 00:21:01 -0500

    Time: 2532 ms (21.81%), Space: 13.9 MB (99.86%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,17 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # create a dp array for 0 to amount (inclusive) with the max values
+        # Create a dp array for 0 to amount (inclusive) with the max values
         dp = [float('inf')] * (amount+1)
+        
+        # Initialize the value of the dp array
         dp[0] = 0
         
-        # let's compute dp array for all values from 1 to amount
+        # Let's compute dp array for all values from 1 to amount
         for i in range(1, amount+1):
-            # we will consider all the coins and then take min from that
+            # We will consider all the coins and then take min from that
             for coin in coins:
                 if i-coin >= 0:
                     dp[i] = min(dp[i], 1 + dp[i-coin])
-        # after this loop is run, dp[amount] is our result given the fact that it's not inf
-        # if it's inf, then we will return -1
+        # After this loop is run, dp[amount] is our result given the fact that it's not infinity
+        # If it's inf, then we will return -1
         return dp[amount] if dp[amount] != float('inf') else -1
         
         
