46bfc6635b8bfd929e77f4f6de5eac2f3d281db1
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-13 13:40:43 -0500

    Time: 93 ms (60.45%), Space: 17.6 MB (63.41%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -2,26 +2,28 @@ from heapq import heapify, heappush, heappop
 class Solution:
     def minMeetingRooms(self, inter: List[List[int]]) -> int:
     
-        # sort the meeting times by start-tiems
+        # Sort the meeting times by start-times in increasing order
         inter.sort()
         
-        # create an end time min-heap which will hold the end time of the last meeting.
-        # in other words this time is telling us that after this time, the room will be availabe
-        # The final result will be the length of such a min-heap
+        '''
+        Create an end time min-heap which will hold the end time of the last meeting.
+        In other words this time is telling us that after this time, the room will be availabe
+        The final result will be the length of such a min-heap
+        '''
         endTimeMinHeap = []
 
         for start, end in inter:
-            # if there is nothing in the min-heap then push the current end time, as that would mean we booked a room
+            # If there is nothing in the min-heap then push the current end time, as that would mean we booked a room
             if len(endTimeMinHeap) == 0:
                 heappush(endTimeMinHeap, end)
                 continue
-            # if the current start time for which we want to book a meeting is less than end time, it means we can't use this room to book a meeting  
-            elif start < endTimeMinHeap[0]: # here endTimeMinHeap[0] is the first element of heap representing top of the min-heap
+            # If the current start time for which we want to book a meeting is less than end time, it means we can't use this room to book a meeting  
+            elif start < endTimeMinHeap[0]: # Here endTimeMinHeap[0] is the first element of heap representing top of the min-heap
                 heappush(endTimeMinHeap, end)
-            # other wise it means that we can book the room. So, we need to update the end time of the top-most element in the min-heap by doing pop and push operation  
+            # Other wise it means that we can book the room. So, we need to update the end time of the top-most element in the min-heap by doing pop and push operation  
             else:
-                heappop(endTimeMinHeap)         # remove the last end time
-                heappush(endTimeMinHeap, end)   # update the end time
+                heappop(endTimeMinHeap)         # Remove the last end time
+                heappush(endTimeMinHeap, end)   # Update the end time
         return len(endTimeMinHeap)
             
     
