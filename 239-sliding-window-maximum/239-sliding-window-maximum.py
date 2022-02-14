7383bd695c0db82feb40b0164262bcf2d315815b
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-14 15:49:44 -0500

    Time: 1752 ms (82.44%), Space: 30.7 MB (15.26%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,18 +1,18 @@
-from heapq import heapify, heappop, heappush
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # we will maintain the queue in monotinic decreasing fashion, so that left value is the max one
-        w = deque()
-        res = []
+        # we will maintain the queue in monotinic decreasing fashion, so that left value in the deque is always the max one
+        w, res = deque() , []
         
         for i,x in enumerate(nums):
-            # If the current val is greater then the right most element in queue, then remove it. 
+            # If the current element is greater then the right most element in the deque, then remove it. 
             # Do it till the current element is less then the last element in the deque
             while w and  x >= w[-1][1]:
                 w.pop()
+                
             # if the current index - index of the left most item in deque is >= K, it means that pair is invalid, so remove it
             while w and i - w[0][0] >= k:
                 w.popleft()    
+                
             # add the index and value pair as tuple at the end of the deque
             w.append((i,x))
             
