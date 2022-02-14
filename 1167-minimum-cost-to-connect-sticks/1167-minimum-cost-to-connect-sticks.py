36ff657899cc09443dd67b87e5682ee56c25f807
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-14 05:50:48 -0500

    Time: 300 ms (97.18%), Space: 14.9 MB (93.41%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
@@ -1,21 +1,28 @@
 from heapq import heapify, heappush, heappop
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
+        # create a min-heap and add the sticks to it
         minCost, minHeap = 0, sticks
         
         # edge case 
         if len(sticks) == 0 or len(sticks) == 1:
             return minCost
         
+        # min-heapify the sticks
         heapify(minHeap)
         
+        # merge sticks unless, minHeap has more than one element
         while(len(minHeap) > 1):
-            x = heappop(minHeap)
-            y = heappop(minHeap)
-            z = x+y
-            minCost += z 
-            heappush(minHeap,z)
-  
+            # get the 2 minimum elements from heap
+            x,y  = heappop(minHeap), heappop(minHeap)
             
+            # merge the smallest sticks
+            z = x+y
+            
+            # update the cost
+            minCost += z 
+            
+            # add the merged stick to the min-heap
+            heappush(minHeap,z)  
         return minCost
             
