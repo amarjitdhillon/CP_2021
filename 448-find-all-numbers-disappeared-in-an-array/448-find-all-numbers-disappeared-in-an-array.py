3bb15c2428f28d6696d048f721f69c0aab950e3c
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-15 14:35:09 -0500

    Time: 729 ms (7.01%), Space: 27 MB (5.70%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -1,5 +1,10 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        # convert the nums to a set to get the unique element 
         setA = set(nums)
+        
+        # create a set consisting of the original elements for this range
         setB = {i for i in range(1,len(nums)+1)}
+        
+        # return the difference of elements which are in setB but not in setA. Note that `setA - setB` wont work in this case
         return(setB - setA)
