c5b984ed0807c93658fd56e993c99ca45725e4e0
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-19 14:38:54 -0500

    Time: 56 ms (98.59%), Space: 14 MB (81.29%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -11,12 +11,16 @@ class Solution:
             
         # populate freq_s2 list
         for x in range(0,len(s2)-len(s1)+1):
+            # if x is 0, then we will loop over the sliding window for the first time
             if x == 0:
-                for y in range(x,x+len(s1)):
+                # if x == 0, then loop over the sliding window
+                for y in range(x , x+len(s1)):
                         freq_s2[ord(s2[y])- ord('a')] +=  1        
-            else: # increment the count for char in end of sliding window
+            # if x != 0, increment the count for char in the end of sliding window (of s1 length)
+            else: 
                 freq_s2[ord(s2[x+len(s1)-1])- ord('a')] +=  1 
                 
+            # if both list are same then we have found the match
             if freq_s2 == freq_s1:
                 return True
             
