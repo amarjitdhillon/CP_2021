a553851edd1e0e1a11f33568c78a1eed7dd0e0b5
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-19 17:06:57 -0500

    Time: 1214 ms (63.33%), Space: 13.9 MB (86.15%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,21 +1,27 @@
 class Solution:
     def longestPalindrome(self, arr: str) -> str:
         # From each number we can go outwards and check if its a valid palindrome
-        res, curLen, resLen = "",0 ,0
+        palindromeString, curLen, resLen = "", 0 , 0
         
-        def findPalindrome(s, e): # s and e are starting and end indexes
-            nonlocal resLen, res
-            while(s >= 0 and e < len(arr) and (arr[s] == arr[e])): # string is a palindrome
-                currLen = e-s+1
+        def findPalindrome(start, end):
+            # These are global variables
+            nonlocal resLen, palindromeString
+            
+            # while the start and end strings are within bounds and are same keep expanding outwards 
+            while(start >= 0 and end < len(arr) and (arr[start] == arr[end])): 
+                # find the current length of Palindrome string
+                currLen = end-start+1
+                
                 if currLen > resLen:
-                    # update both the resLen and res as currLen > resLen
-                    res = arr[s:e+1]
+                    # update both the resLen and palindromeString as we have found a new string with more length then older one
+                    palindromeString = arr[start:end+1]
                     resLen = currLen
-                s -= 1  # move `s` to left
-                e += 1  # move `e` to right
+                start   -= 1  # move `start` to right
+                end     += 1  # move `end` to left
         
+        # driver code 
         for i in range(len(arr)):
-            findPalindrome(i,i)   # odd length case
-            findPalindrome(i,i+1) # even length case      
-        return res
+            findPalindrome(i,i)     # For odd length case, set start and end as same index 
+            findPalindrome(i,i+1)   # even length case, set start and end as adjoining indexes      
+        return palindromeString
             
