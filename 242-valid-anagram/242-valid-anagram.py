a91b064f659ce3beea4ffd753d558bce7a8c06d5
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-19 14:09:01 -0500

    Time: 90 ms (22.13%), Space: 14.5 MB (49.67%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,21 +1,31 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # as s and t consist of lowercase English letters, so we can solve this problem in 2 ways
-            # 1. Create 2 list of 26 length and compare them at the end, if they are same --> words are anagram
-            # 2. Second approach (better) is to use just one list of 0 values. For first word increment the count for each char and for another word decrement the counter for each char. If the counter at any character is < 0, it means that the word is not an anagram and thus return False. Else return True at end of second iteration
+        '''        
+        As s and t consist of lowercase English letters, so we can solve this problem in 2 ways
+            1. First approach: Create 2 lists of 26 length and compare them at the end, if they are same then the words are anagram.
+            2. Second approach (better). Here we will use just one list of 26 length initialized to 0 values. 
+                - For first char in first word increment the count for each char and for another char in second word, decrement the counter for each char.
+                - If the counter at any character is < 0, it means that the word is not an anagram and thus return False. Else return True at end of second iteration
+        '''
         
-        # lengths of both should words should be same
+        # Using intutuion: lengths of both should words should be same
         if len(s) != len(t):
             return False
         
+        # initialize the list of length 26 
         charList= [0]*26
         
+        # loop over the first word and populate the charList 
         for i in s: 
-            charList[(ord(i)- ord('a'))] += 1  # increment the counter in first iteration
+            # increment the counter in first iteration
+            charList[(ord(i)- ord('a'))] += 1  
         
+        # loop over the second word to check if both words are same or not
         for i in t: 
-            charList[(ord(i)- ord('a'))] -= 1  # decrement the counter in second iteration
+            # decrement the counter for each character
+            charList[(ord(i)- ord('a'))] -= 1  
+            # if any count is less then 0 for the second word, it means both words are not unique
             if  charList[(ord(i)- ord('a'))] < 0:
                 return False
-        
+        # return True in the end if words are found same
         return True 
