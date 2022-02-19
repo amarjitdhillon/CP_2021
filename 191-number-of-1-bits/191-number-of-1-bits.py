1d0139a55f386140fbfa25e1494d69dc12f47933
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-19 14:21:41 -0500

    Time: 30 ms (84.63%), Space: 13.9 MB (65.78%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,8 +1,12 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
+        # We will right shift the digits by one and increase the counter if the `bitwise-and` of this number is 1
+        # This operation will be done unless the value is > 0
         while(n > 0):
-            if n&1 == 1:   #means the last bit is one, so increase the counter
+            # if the `bitwise-and` with digit 1 is 1, it means that the last bit is one, so we will increase the counter
+            if n&1 == 1:   
                 count += 1
-            n >>= 1         # right shift by one, to process the next bit
+         # right shift by one digit, to process the next bit in the next iteration
+            n >>= 1        
         return count
