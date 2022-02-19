9807a8c8e412f7c8e70c3fdfb0ec9699f5cca0e6
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-19 13:25:14 -0500

    Time: 160 ms (67.66%), Space: 14.7 MB (31.48%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,21 +1,30 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        l, h = 0, len(s)-1 # use 2 pointers
+        # We will use 2 pointers, low and high where both are pointing to begin and end of list respectively
+        low, high = 0, len(s)-1 
   
-        def isPalindrome(i,j): # used to check in any string is a palindrome
+        # This function is used to check in any string is a palindrome or not based on the low and high index
+        def isPalindrome(i,j): 
             while i < j :
                 if s[i] == s[j]:
                     i += 1
                     j -= 1
                 else:
-                    return False # as chars don't match, so we return False
+                    # as chars don't match, so we return False
+                    return False 
+            # if we reach here it  means sting is traversed completely and all values are matching
             return True
         
-        while (l<h):
-            if s[l] == s[h]:
-                l += 1
-                h -= 1
-            else: # we will consider 2 cases, in first we will skip the l^th characher and in other we will skip h^th char
-                return isPalindrome(l+1, h) or isPalindrome(l, h-1) # if either of then return true, then string is a palindrome
+        # driver code starts here
+        while (low < high):
+            if s[low] == s[high]:
+                low += 1        # move low to right
+                high -= 1       # move high pointer towards left
+            # If both chars don't match, thne we will consider 2 cases
+            # In first case, we will skip the low^th characher and in other we will skip high^th char
+            # We will return true if either of the case is returning a valid palindrome
+            else: 
+                # if either of then return true, then string is a palindrome
+                return isPalindrome(low+1, high) or isPalindrome(low, high-1) 
         return True
                 
