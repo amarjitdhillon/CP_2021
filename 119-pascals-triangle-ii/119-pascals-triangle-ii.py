e5edf979ad5e57633c105fd93527609a7d6f719f
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-20 10:51:25 -0500

    Time: 43 ms (47.59%), Space: 13.9 MB (88.90%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        # # edge case scenario
+        # if rowIndex == 0:
+        #     return []
+
+        # create a pascal triangle and then add each row one by one to it
+        pascal, resut = []  , [] 
+        
+        for x in range(rowIndex+1):
+            # we will build a row first. The first row will be of size 1 and later will be of size 2, 3, 4 etc
+            row = [None for _ in range(x+1)]
+            
+            # the first and last cols are always 1 for any given row
+            row[0], row[-1] = 1, 1 
+            
+            # we have to update the values from index 1 to len(row)-1 using DP such that the current val is sum of two previous elements from upper row
+            for y in range(1,len(row)-1):
+                # here the values for row are taken from the pascal triangle. Both of these values are from one row above (using `x-1` here)
+                row[y] = pascal[x-1][y-1] + pascal[x-1][y]
+            
+            # append the row to triangle
+            pascal.append(row)
+            
+            # if x == rowIndex:
+                # result = row
+        return row
