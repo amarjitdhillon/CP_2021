3d005c35413601de251f8e7fd56240a3fb54ba1c
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-21 11:18:00 -0500

    Time: 86 ms (59.45%), Space: 17.9 MB (76.28%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -10,28 +10,29 @@ class MinStack:
     
     # push(int val) pushes the element val onto the stack.
     def push(self, val: int) -> None:
-        # Always add the number in the main stack.
+        # We will always add the number in the main stack.
         self.stack.append(val)
         
-         # If this is the min stack is empty or this is the smaller nunber then current top in main stack, then add this value to minstack 
+         # If the value is the min-stack is empty or current val is the smaller number then current top in min-stack, then add this value to min-stack with counter of 1 
         if len(self.mstack) == 0 or val < self.mstack[-1][0]:
             self.mstack.append([val,1])
-            # Else if this number is equal to what's currently at the top of the min stack, then increment the count at the top by 1.
+            
+            # Else if the val is equal to what's currently at the top of the min stack, then increment the counter at the top by 1.
         elif val == self.mstack[-1][0]:
             self.mstack[-1][1] += 1
 
     # pop() removes the element on the top of the stack.
     def pop(self) -> None:
-        # we need to compare the top of min stack and top of main stack
-        # if the top of min stack is same as the top of the main stack, then we need to decrement the count
+        # We need to compare the top of min-stack and top of main-stack
+        # if the top of min-stack is same as the top of the main-stack, then we need to decrement the count in the min-stack
         if self.mstack[-1][0] == self.stack[-1]:
             self.mstack[-1][1] -= 1
             
-        # if the count at the top of the min stack is 0, then remove it
+        # if the count at the top of the min-stack is 0, then remove that value pair
         if self.mstack[-1][1] == 0:
             self.mstack.pop()
             
-        # pop value from the main stack
+        # Pop value from the main stack as this is the pop operation
         self.stack.pop()
         
     
