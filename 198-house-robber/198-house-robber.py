2a0204ff086ecec2c18969a5c1f0600c5badf8f3
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-24 06:11:13 -0500

    Time: 32 ms (86.12%), Space: 13.8 MB (99.17%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -4,9 +4,12 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        rob1, rob2 = 0, 0
+        secondLastRob, lastRob = 0, 0
         for rob in nums:
-            curr = max(rob1+ rob, rob2)
-            rob1 = rob2
-            rob2 = curr
-        return rob2
+            # our current rob can include current house + all houses excluding last rob OR the last robbed values as we are excluding the current rob
+            currRob         = max(secondLastRob+rob, lastRob)
+            # update the last robbed sum for next iteration
+            secondLastRob   = lastRob
+            # update the last rob sum for next iteration
+            lastRob         = currRob
+        return lastRob                      # as the lastRob was updated to currRob at the end of the iteration
