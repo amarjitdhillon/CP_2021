9122a1118ab1e43e8ca4b21603f843238e6be4e1
Amarjit Singh Dhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-24 07:33:25 -0500

    Time: 34 ms (74.35%), Space: 13.9 MB (68.28%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -7,7 +7,6 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        
         # function to rob a neighbourhood in a straight line
         def robHouse(houses) -> int:
             prevRob, SecondPrevRob, currRob = 0, 0, 0
@@ -19,11 +18,14 @@ class Solution:
                 # update values for next iteration
                 SecondPrevRob   = prevRob
                 prevRob         = currRob
-            # at the end of iterationm, the prevRob was updated to currRob, so we can return either of these
-            return prevRob      
+                
+            # at the end of iteration, the prevRob was updated to currRob, so we can return either of these
+            return currRob      
         
-        # If we choose to rob first house (0 index) then we can't rob the last one as both are connected, so it means we can rob from 0 to N-1. In Python slicing [0:N] means N is not included
-        # if we choose NOT to rob the first house, then we can rob from 1 to N
-        # The final result will be max of both of these robs
-        N = len(nums) -1       
+        '''
+        If we choose to rob first house (0 index) then we can't rob the last one as both are connected, so it means we can rob from 0 to N-1. In Python slicing [0:N] means N is not included. 
+        iIf we choose NOT to rob the first house, then we can rob from 1 to N
+        The final result will be max of both of these robs
+        '''
+        N = len(nums)-1       
         return max(robHouse(nums[0:N]) , robHouse(nums[1:]) )
