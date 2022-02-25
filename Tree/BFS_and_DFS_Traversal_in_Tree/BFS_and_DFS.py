ef33fc7878409020f5010e6ce8fbee278c139042
amarjitdhillon <amarjitdhillon@cmail.carleton.ca>
2022-02-25 12:36:54 -0500

    BFS and DFS in a Tree

--- Tree/BFS_and_DFS_Traversal_in_Tree/BFS_and_DFS.py
+++ Tree/BFS_and_DFS_Traversal_in_Tree/BFS_and_DFS.py
@@ -1,25 +1,37 @@
 class Node:
 
+    # Create a tree node as per given data
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
+    # add node to the tree based on the BST login
     def add(self,data):
-        if self.data != None: # checking if the data is not null
-            if data < self.data: # we can insert in left
-                if self.left is None: # means we can insert in left
+
+        # check if the data is not null
+        if self.data != None:
+            # we can insert in left if this number is less then root's value
+            if data < self.data:
+                # we can insert in left if there is no node
+                if self.left is None:
                     self.left = Node(data)
                 else:
+                    # else call this function recursively
                     self.left.add(data)
-            elif data > self.data: # we need to insert in the right
-                if self.right is None: # add it to the left immediately
+            # we need to insert in the right if this value is higher
+            elif data > self.data:
+                # if this value is higher, so we can add it to the right side
+                if self.right is None:
                     self.right = Node(data)
                 else:
+                    # call this function recursively
                     self.right.add(data)
-            else: # we need to add data  at this root
+            # we need to add data  at this root as no node existed
+            else:
                 self.data = Node(data)
 
+    # function to do inorder traversal: left --> root --> right
     def inorderTraversal(self, root):
         res = []
         if root:
@@ -28,6 +40,7 @@ class Node:
             res += self.inorderTraversal(root.right)
         return res
 
+    # do the pre-order traversal: root --> left --> right
     def preorderTraversal(self, root):
         res = []
         if root:
@@ -36,6 +49,7 @@ class Node:
             res += self.preorderTraversal(root.right)
         return res
 
+    # postorder traversal: left --> right --> root
     def postorderTraversal(self,root):
         res = []
         if root:
@@ -44,6 +58,7 @@ class Node:
             res.append(root.data)
         return res
 
+    # Here, we will need a queue to do the BFS.
     def bfs(self,root):
         queue = []
         res = []   # for string the output
@@ -51,10 +66,12 @@ class Node:
             queue.append(root)
 
         while len(queue) > 0:
-            first = queue.pop(0)     # get the first element from the queue
+            # get the first element from the queue
+            first = queue.pop(0)
             res.append(first.data)
 
-            left = first.left       # get the left and right nodes of the first element
+            # get the left and right nodes of the first element
+            left = first.left
             right = first.right
 
             if left:
@@ -64,7 +81,10 @@ class Node:
         return res
 
 if __name__ == '__main__':
+    # create a tree node
     tree = Node(12)
+
+    # add elements to the tree node
     tree.add(6)
     tree.add(14)
     tree.add(3)
@@ -72,6 +92,15 @@ if __name__ == '__main__':
     tree.add(13)
     tree.add(15)
 
+    '''
+    Tree will be like
+                      12
+                    /   \
+                   6     14
+                  / \    /  \
+                3    7  13  15
+    '''
+    # print various traversals
     print("  In order =", tree.inorderTraversal(tree))
     print(" Pre order =", tree.preorderTraversal(tree))
     print("Post order =", tree.postorderTraversal(tree))
---
+++ Tree/BFS_and_DFS_Traversal_in_Tree/readme.md
@@ -0,0 +1,11 @@
+# DFS and BFS traversal in tree
+
+## The problems done in this question are:
+
+- Add the nodes to the tree to make a binary tree
+- Perfrom in-order DFS traversal
+- Perfrom pre-order DFS traversal
+- Perfrom post-order DFS traversal
+- Perform BFS traversal
+
+
